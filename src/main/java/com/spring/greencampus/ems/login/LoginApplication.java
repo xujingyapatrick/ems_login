acc854c752ad6a609b4250ac94b7caeea329fb2f
xujingyapatrick <xujingyastan@gmail.com>
2017-12-14 11:05:05 -0800

    with gitignore

--- src/main/java/com/spring/greencampus/ems/login/LoginApplication.java
+++ src/main/java/com/spring/greencampus/ems/login/LoginApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginApplication {
 
+	
 	public static void main(String[] args) {
+
+		System.out.println("ddenaiufuefbueyfbtyuegf");
 		SpringApplication.run(LoginApplication.class, args);
 	}
 }
